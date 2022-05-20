55a18e1bc78184f007c239799aa9805e5dfd64fb
lidiya yohannes <lilujoli65@gmail.com>
2022-05-20 10:24:34 -0700

    updated

--- shell.h
+++ shell.h
@@ -1,112 +1,44 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
-
-/**###### environ var ######*/
-
-extern char **environ;
-
-/**##### MACROS ######*/
-
-#define BUFSIZE 1024
-#define DELIM " \t\r\n\a"
-#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
-
-/**###### LIBS USED ######*/
-
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <linux/limits.h>
-
-
-
-
-
-/**###### STRING FUNCTION ######*/
-
-char *_strtok(char *str, const char *tok);
-unsigned int check_delim(char c, const char *str);
-char *_strncpy(char *dest, char *src, int n);
-int _strlen(char *s);
-int _putchar(char c);
-int _atoi(char *s);
-void _puts(char *str);
-int _strcmp(char *s1, char *s2);
-int _isalpha(int c);
-void array_rev(char *arr, int len);
-int intlen(int num);
-char *_itoa(unsigned int n);
-char *_strcat(char *dest, char *src);
-char *_strcpy(char *dest, char *src);
-char *_strchr(char *s, char c);
-int _strncmp(const char *s1, const char *s2, size_t n);
-char *_strdup(char *str);
-
-/**###### MEMORIE  MANGMENT ####*/
-
-void free_env(char **env);
-void *fill_an_array(void *a, int el, unsigned int len);
-char *_memcpy(char *dest, char *src, unsigned int n);
-void *_calloc(unsigned int size);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void free_all(char **input, char *line);
-
-/**###### INPUT Function ######*/
-
-void prompt(void);
-void signal_to_handel(int sig);
-char *_getline(void);
-
-/** ###### Command parser and extractor ###*/
-
-int path_cmd(char **line);
-char *_getenv(char *name);
-char **parse_cmd(char *cmd);
-int handle_builtin(char **cmd, int er);
-void read_file(char *filename, char **argv);
-char *build(char *token, char *value);
-int check_builtin(char **cmd);
-void creat_envi(char **envi);
-int check_cmd(char **tokens, char *line, int count, char **argv);
-void treat_file(char *line, int counter, FILE *fd, char **argv);
-void exit_bul_for_file(char **cmd, char *line, FILE *fd);
-
-/** ####BUL FUNC #####*/
-
-void hashtag_handle(char *buff);
-int history(char *input);
-int history_dis(char **cmd, int er);
-int dis_env(char **cmd, int er);
-int change_dir(char **cmd, int er);
-int display_help(char **cmd, int er);
-int echo_bul(char **cmd, int er);
-void  exit_bul(char **cmd, char *input, char **argv, int c);
-int print_echo(char **cmd);
-
-/** ####error handle and Printer ####*/
-void print_number(unsigned int n);
-void print_number_in(int n);
-void print_error(char *line, int c, char **argv);
-void _prerror(char **argv, int c, char **cmd);
-
-
+#ifndef SHELL_H
+#define SHELL_H
 /**
- * struct bulltin - contain bultin to handle and function to excute
- * @command:pointer to char
- * @fun:fun to excute when bultin true
+ * Header File - shell.h
  */
-
-typedef struct  bulltin
-{
-	char *command;
-	int (*fun)(char **line, int er);
-} bul_t;
-
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <limits.h>
+#define CHANGE_DIR 1
+#define EXIT_SHELL 2
+#define PERM_DENIED "Permission denied"
+#define NOT_FOUND "not found"
+int _atoi(char *s);
+void removeSpaces(char *str);
+void reverse(char *str, int len);
+int intToStr(int x, char str[]);
+void _puts(char *str);
+void errors(char *program, char *param, char *message, int Qexe);
+void intoHsh(char **env, char **argv);
+char **ParseCommand(char *command, char *separator);
+void _prompt(void);
+int _exec(char **param, char **env);
+void _printenv(char **env);
+int Builtin(char *command, char **env, int status);
+char *_GetEnv(char *var, char **env);
+int _path(char **param, char **env);
+void execute(char *command, char **env);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+char *_strdup(char *str);
+char *_strchr(char *s, char c);
+int _strcmp(char *s1, char *s2);
+int _Wait(char **argv, char *command, int QExecutes);
+int BuiltExit(char *command, int status);
+int _strlen(char *s);
+int _strncmp(char *s1, char *s2, int n);
+int _putchar(char c);
 #endif
